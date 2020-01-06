d3f0558534f6e85063cd5f5ef55f345683f7817d
Carlos <fer.1972@hotmail.com>
2020-01-06 11:31:41 -0300

    Classes

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-class Teste {
-    method(){
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    otherMethod(){
-
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+    constructor(){
+        super();
+
+        this.usuario = 'Macacudo';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+
+class Math {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+console.log(Math.soma(1, 2));
