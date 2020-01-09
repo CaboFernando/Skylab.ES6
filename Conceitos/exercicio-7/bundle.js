65d40150d10f8366b9333d9563e8285e99982a46
Carlos <fer.1972@hotmail.com>
2020-01-09 11:38:16 -0300

    Exercício 7

---
+++ Conceitos/exercicio-7/bundle.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var nome = 'Diego';
+var idade = 23; // const usuario = {
+//     nome: nome,
+//     idade: idade,
+//     cidade: 'Rio do Sul',
+// };
+
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
+console.log(usuario);
---
+++ Conceitos/exercicio-7/exercicio-7.js
@@ -0,0 +1,14 @@
+const nome = 'Diego';
+const idade = 23;
+// const usuario = {
+//     nome: nome,
+//     idade: idade,
+//     cidade: 'Rio do Sul',
+// };
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul',
+};
+
+console.log(usuario);
