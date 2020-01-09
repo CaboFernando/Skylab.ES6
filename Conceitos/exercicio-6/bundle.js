361e6457a366322a3b609c3ebfe69aaa212802e7
Carlos <fer.1972@hotmail.com>
2020-01-09 11:34:31 -0300

    Exercício 6

---
+++ Conceitos/exercicio-6/bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var usuario = 'Diego';
+var idade = 23; //console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+
+console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
